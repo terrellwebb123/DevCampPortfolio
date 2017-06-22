0fa3661ffc8ee54ca741b47afd06d60cbc84a327
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:46:26 +0000

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ puts "10 blog posts created"
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       main_image: "http://placehold.it/600x400",
-      thumb_image: 'http://placehold.it/350x150'
+      thumb_image: 'http://placehold.it/350x200'
       )
   end
   
